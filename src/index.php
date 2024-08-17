cd0ba95e8234b6ca55cc3cf5606ccae8ee0a1047
yaonglax <stasya200411@gmail.com>
2024-08-17 21:12:46 +0300

    last commit

--- src/index.php
+++ src/index.php
@@ -8,8 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
 }
-
+require 'vendor/autoload.php';
 require_once('./phpmailer/PHPMailerAutoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
@@ -17,15 +21,15 @@ $message = $_POST['popup-textarea'];
 
 
 $mail->isSMTP();                                    
-$mail->Host = 'smtp.yandex.ru';  																						
+$mail->Host = $_ENV['SMTP_HOST'];  																						
 $mail->SMTPAuth = true;                          
-$mail->Username = 'exampleyaon@yandex.com'; 
-$mail->Password = 'icmguaxxulanpufm'; 
+$mail->Username = $_ENV['SMTP_USER']; 
+$mail->Password = $_ENV['SMTP_PASS']; 
 $mail->SMTPSecure = 'ssl';                     
-$mail->Port = 465; 
+$mail->Port = $_ENV['SMTP_PORT']; 
 
-$mail->setFrom('exampleyaon@yandex.com');
-$mail->addAddress('rbru-metrika@yandex.ru');   
+$mail->setFrom($_ENV['SMTP_USER']);
+$mail->addAddress($_ENV['SMTP_EMAILTO']);   
 $mail->isHTML(true);                               
 
 $mail->Subject = 'Сообщение с сайта';
